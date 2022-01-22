00935074b65590c7b31c0c7f77c7eb9005198a46
naknak92 <rafaelnakache@gmail.com>
2022-01-22 14:33:27 +0100

    login

--- login.php
+++ login.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html>
 <html>
 
+
 <head>
-    <link rel="stylesheet" href="style.css" />
+	<title>Slide Navbar</title>
+	<link rel="stylesheet" type="text/css" href="Style3.css">
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
+<body>
 </head>
 
 <body>
@@ -19,29 +23,43 @@
         $username = mysqli_real_escape_string($conn, $username);
         $password = stripslashes($_REQUEST['password']);
         $password = mysqli_real_escape_string($conn, $password);
-        $query = "SELECT * FROM `users` WHERE username='$username' and password='" . hash('sha256', $password) . "'";
+        $query = "SELECT * FROM users WHERE username='$username' and password='" . hash('sha256', $password) . "'";
         $result = mysqli_query($conn, $query) or die(mysqli_error($conn, $query));
         $rows = mysqli_num_rows($result);
         if ($rows == 1) {
             $_SESSION['username'] = $username;
-            $rows=mysqli_fetch_array($result);
-            $_SESSION['ID'] =  $rows[0];
             header("Location: index.php");
         } else {
             $message = "Le nom d'utilisateur ou le mot de passe est incorrect.";
         }
     }
     ?>
-    <form class="box" action="" method="post" name="login">
-        <h1 class="box-title">Connexion</h1>
-        <input type="text" class="box-input" name="username" placeholder="Nom d'utilisateur">
-        <input type="password" class="box-input" name="password" placeholder="Mot de passe">
-        <input type="submit" value="Connexion " name="submit" class="box-button">
-        <p class="box-register">Vous êtes nouveau ici? <a href="register.php">S'inscrire</a></p>
-        <?php if (!empty($message)) { ?>
-            <p class="errorMessage"><?php echo $message; ?></p>
-        <?php } ?>
-    </form>
-</body>
 
+
+
+
+
+	<div class="main">  	
+		<input type="checkbox" id="chk" aria-hidden="true">
+
+			<div class="signup">
+				<form>
+					<label for="chk" aria-hidden="true">Sign up</label>
+					<input type="text" name="txt" placeholder="User name" required="">
+					<input type="email" name="email" placeholder="Email" required="">
+					<input type="password" name="pswd" placeholder="Password" required="">
+					<button>Sign up</button>
+				</form>
+			</div>
+
+			<div class="login">
+				<form>
+					<label for="chk" aria-hidden="true">Login</label>
+					<input type="email" name="email" placeholder="Email" required="">
+					<input type="password" name="pswd" placeholder="Password" required="">
+					<button>Login</button>
+				</form>
+			</div>
+	</div>
+</body>
 </html>
